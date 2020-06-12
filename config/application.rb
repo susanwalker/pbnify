8f450b000a3741d6550b953990229b0d7c55e88c
susanwalker <susanwalekr8@gmail.com>
2020-06-12 16:02:11 -0400

    Require byebug in application

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'byebug'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
